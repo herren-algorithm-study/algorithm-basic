5229681736829bc6ae5caf4f71de9d3102b33483
yunjey0 <sehajyang@gmail.com>
2020-01-14 00:45:16 +0900

    fix : solution9613 디버깅 라인 제거

--- seha/solution9613.py
+++ seha/solution9613.py
@@ -15,7 +15,5 @@ if __name__ == '__main__':
         cnt = 0
         for i in range(0, s - 1):  # 마지막수는 안돌아도 되기 때문에 s - 1
             for j in range(i + 1, s): # 끝까지 다 조합해봐야 됨. 단, 이전에 체크한건 다시 체크할 필요 없음
-                rst = gcd(int(i), int(j))
-                cnt += rst
-                print(i, j, rst)
+                cnt += gcd(int(i), int(j))
         print(cnt)
