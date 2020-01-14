ac4c388e8d7ced37958f80dc6ee84c32bface786
yunjey0 <sehajyang@gmail.com>
2020-01-15 00:35:55 +0900

    feat(chapter2) : upload solution1182

--- seha/chapter1/solution9613.py
+++ seha/chapter1/solution9613.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
         s, *num_list = map(int, input().split())
         cnt = 0
         for i in range(0, s - 1):  # 마지막수는 안돌아도 되기 때문에 s - 1
-            for j in range(i + 1, s): # 끝까지 다 조합해봐야 됨. 단, 이전에 체크한건 다시 체크할 필요 없음
+            for j in range(i + 1, s):  # 끝까지 다 조합해봐야 됨. 단, 이전에 체크한건 다시 체크할 필요 없음
                 cnt += gcd(int(i), int(j))
         print(cnt)
---
+++ seha/chapter2/solution1182.py
@@ -0,0 +1,20 @@
+"""
+https://www.acmicpc.net/problem/1182
+"""
+
+from itertools import combinations
+
+
+def solve(n1_list, n2_list):
+    count = 0
+    for i in range(1, n1_list[0] + 1):
+        for item in combinations(n2_list, i):
+            if sum(item) == n1_list[1]:
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    n1_list = list(map(int, input().split()))
+    n2_list = list(map(int, input().split()))
+    print(solve(n1_list, n2_list))
